1cad5021d0a8e81e237e2377536c27d8e54438fb
unknown <chaitanya.pandey072gmail.com>
2025-08-24 15:02:04 +0530

    exploring get, post

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 
 const app = express();
 
+// this will only handle get call to /user
+app.get("/user", (req, res) => {
+    res.send({firstName: "chai", lastName: "pan"})
+})
+
+app.post("/user", (req, res) => {
+    res.send("user deleted successfully");
+})
+
+// this will match all the htp method api calls to /home
 app.use("/home", (req, res) => {
     res.send("This is my home page");
-});
-
-app.use("/dashboard", (req, res) => {
-    res.send("This is dashboard page");
 });
 
 app.listen(3000, () => {

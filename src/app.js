c8ff53cfcf885cf5f0066f6acb09174e45e57570
unknown <chaitanya.pandey072gmail.com>
2025-08-24 15:29:11 +0530

    test params urls

--- src/app.js
+++ src/app.js
@@ -3,13 +3,31 @@ const express = require("express");
 const app = express();
 
 // this will only handle get call to /user
-app.get("/user", (req, res) => {
+// app.get(/.*fly$/, (req, res) => {
+//     res.send({firstName: "chai", lastName: "pan"})
+// })
+
+// b is optional here, /ac /abc
+// app.get("/ab*c", (req, res) => {
+//     res.send({firstName: "chai", lastName: "pan"})
+// });
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params)
+
     res.send({firstName: "chai", lastName: "pan"})
 })
 
+
+app.get("/poster", (req, res) => {
+    console.log(req.query)
+    res.send({firstName: "chai", lastName: "pan"})
+})
+
+
 app.post("/user", (req, res) => {
     res.send("user deleted successfully");
-})
+});
 
 // this will match all the htp method api calls to /home
 app.use("/home", (req, res) => {
